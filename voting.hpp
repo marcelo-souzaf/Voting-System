0e18308d376e2dfe90522b843a21da46175ed1f2
Lfzinho <laguardialuis@gmail.com>
2022-10-01 00:58:57 -0300

    adiciona a func filter_by_state

--- voting.hpp
+++ voting.hpp
@@ -175,6 +175,19 @@ class Roberto {
         return votecount;
     }
 
+    Vector<Vote> filter_by_state(Vector<Vote>, char* state) {
+        Vector<Vote> filtered;
+        for (uint i = 0; i < this->current_idx; i++) {
+            if (this->data[i].get_user_id() != 0) {
+                if (this->data[i].get_abbrev()[0] == state[0] &&
+                    this->data[i].get_abbrev()[1] == state[1]) {
+                    filtered.push_back(this->data[i]);
+                }
+            }
+        }
+        return filtered;
+    }
+
  private:
     void quicksort(Vote* data, int left, int right) {
         int i = left, j = right;
